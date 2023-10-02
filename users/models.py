52bffa3e865865592fc75a17dd5235a11380a9cb
iPoetDev <51715025+iPoetDev@users.noreply.github.com>
2023-10-02 12:08:48 +0100

    feat (apps) Custom Model/Manager for users
    Previous: 2023-10-02 00.00.00.001
    Changelog: 2023-10-02 00.00.00.002
    Replica: https://github.com/ipoetdev/dash-and-do
    Replicat: Branch: epic-kore
    Intent: Setting up Admin with custom user and manager
    Goal: To add a user to the database via Admin or replciate bug in admin
    
    AI Notes
    Add custom user model and registration system
    
    Added a custom user model to handle user authentication and registration. It includes changes to the settings file, where the default User model was replaced with the new one; and creation of a manager for the model that defines methods required to create instances of 'User'. This update is necessary to have more control over user information and authentication process. This also includes creation of a new app for managing user activities. The Django-allauth package used to handle user registration and auth related tasks. These modifications were created on the basis of requirements for managing user registration and profile management in a more customized and flexible manner.

--- users/models.py
+++ users/models.py
@@ -1,4 +1,87 @@
-""" doc """
+#!/user/bin/env python3
+"""
+    @File: <filename>.py
+    @Version: 0.3.0 to 0.3.0.?
+    @Desc: apps | <app> |  <module>
+    @Author: Charles Fowler
+    @Copyright: 2023
+    @Date Created: 23/09/?
+    @Date Modified: 23/09/??
+    @Python Version: 3.11.04
+    @Django Version: 4.2.3/.04/.05
+    @Notes / Ideas v Implement:
+        - .
+    @Changelog:
+    - Added:
+        - added: Created initial file: 23/09/??:
+    - Updated:
+        - updated:
+    @Plan:
+        - TODO:
+        - FIXME:
+        - CHECK:
+"""
+
+from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import PermissionsMixin
 from django.db import models
 
-# Create your models here.
+from users.managers import DashUserManager
+
+
+class DashUser(AbstractBaseUser, PermissionsMixin):
+    """Credit:
+    - Author: Sarthak Kumar(@ksarthak4ever)
+    - Title: Django Custom User Model + Allauth for OAuth
+    - Date Created: 2019-04-02
+    - URL: https://medium.com/
+    - USER: @ksarthak4ever/
+    - SLUG: django-custom-user-model-allauth-for-oauth-20c84888c3184
+    - Last Accessed: 2023-09-24
+    :class: DashUser: Custom User Model, subclasses: AbstractBaseUser
+    :param email:
+    :param name:
+    :param is_staff: required by admin
+    :param is_superuser: PermissionsMixin to grant all permissions
+    :param is_active: user active?
+    :param last_login:
+    :param date_joined:
+    :param USERNAME_FIELD: unique id, i.e email
+    :param EMAIL_FIELD: re get_email_field_name()
+    :param REQUIRED_FIELDS: list of required fields on sigh up
+    :param objects: User Manager
+    :return: User
+    :rtype: User.
+    """
+
+    class Meta:  # pylint: disable=R0903
+        """Meta definition for DashUser."""
+        db_table = 'dashuser'
+        app_label = 'users'
+        verbose_name = 'DashUser'
+
+    email = models.EmailField(max_length=254, unique=True)
+    name = models.CharField(max_length=254, null=True, blank=True)
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    last_login = models.DateTimeField(null=True, blank=True)
+    date_joined = models.DateTimeField(auto_now_add=True)
+
+    USERNAME_FIELD = 'email'
+    EMAIL_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    objects = DashUserManager()
+
+    def get_absolute_url(self):
+        """Return the absolute URL for the DashUser instance.
+
+        :return: The absolute URL for the DashUser instance.
+        """
+        return f"/users/{self.pk}/"
+
+    def __str__(self):
+        """Return the string representation of the DashUser instance."""
+        return f"{self.name} ({self.email})" \
+            if self.name else self.email or "No email"
